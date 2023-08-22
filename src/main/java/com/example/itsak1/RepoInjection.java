6c57a4c78b68347ebe5b55b06db40693e448b2ec
Marica Björklund <marica.bjorklund@hotmail.com>
2023-08-22 12:38:30 +0200

    Injection done

---
+++ src/main/java/com/example/itsak1/RepoInjection.java
@@ -0,0 +1,45 @@
+package com.example.itsak1;
+
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.List;
+import java.util.Properties;
+
+public class RepoInjection {
+
+    private Properties properties = new Properties();
+
+    public RepoInjection(){
+        try{
+            properties.load(new FileInputStream("src/main/java/com/example/itsak1/settings.properties"));
+        }catch (Exception e){
+            System.out.println("Failed to read properties");
+        }
+    }
+
+    public User login(String name, String password){
+
+        String sql = "select * from itsak1.user where username = '"+ name + "' and password = '"+ password + "'";
+        System.out.println(sql);
+        try(Connection con = DriverManager.getConnection(
+                properties.getProperty("connection"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
+            Statement stmn = con.createStatement();
+            ResultSet rs = stmn.executeQuery(sql)) {
+
+            while (rs.next()){
+                int id = rs.getInt("user.id");
+                String userName = rs.getString("user.username");
+                String pass = rs.getString("user.password");
+                return new User(id, userName, pass);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return new User();
+    }
+
+}
